925ba0af6fe2046251398b364e8a4e02813c301d
yake2024 <yakeabou209@gmail.com>
2024-07-10 11:22:45 +0000

    Update main.py

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,6 @@
-# Écrivez votre code ici !
+nom = "SOUMAHORO"
+prenom = "Yaké Abou"
+age = 23
+print(f"je m'appelle {nom} {penom} et j'ai {age} ans.")
+prenom = "Aboubacar"
+print(f"je m'appelle {nom} {penom} et j'ai {age} ans.")
